91500649b2deed4ad631ea2930e8d755fa356996
Emilly Furtunato <emillyfurtunato2008@gmail.com>
2024-09-11 14:06:38 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# atribuição de variáveis
+nome = "Emilly"
+idade = 16
+altura = 1.57
+
+# exibir mensagem na tela
+print(" nome: ", nome, "idade: ",idade,"altura: ",altura,)
+# formatação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
